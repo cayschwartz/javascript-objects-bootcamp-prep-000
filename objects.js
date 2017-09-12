3a89fcd4850af5a94c64f075f91d69c304ca1e40
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 16:23:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,15 +4,9 @@ var playlist = {JohnMayer: "In the Blood"};
 function updatePlaylist(obj, artist, song){
   obj[artist]=song
   return obj
-
 }
 
 function removeFromPlaylist(obj, artist){
-
-  //var temp = Object.assign({},obj);
-  //temp.Mastery = 'Songy'
   delete obj[artist]
   return obj
-  //obj = temp
-  //return obj
 }
