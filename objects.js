a782035582a0347046b438b5d2267c2232d84972
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 16:01:59 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+var playlist = {JohnMayer: "In the Blood"};
+
+
+function updatePlaylist(obj, artist, song){
+  obj[artist]=song
+  return obj
+  
+}
+
+function removeFromPlaylist(obj, artist){
+  Delete 
+  return obj
+}
